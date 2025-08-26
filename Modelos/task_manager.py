307c67ca69a372fdf30d24c07269c2c24307822b
tolive75 <tolive75@ford.com>
2025-08-26 00:45:40 -0300

    teste20

--- Modelos/task_manager.py
+++ Modelos/task_manager.py
@@ -7,7 +7,7 @@ class TaskManager:
 
     def __init__(self):
         self.tarefas = []
-        #teste17
+        #teste20
 
 
     def adicionar_tarefa(self, titulo, descricao, prioridade="------"):
