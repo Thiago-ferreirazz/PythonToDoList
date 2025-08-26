67a204ae4e41de23890b57d5486905a6afa13464
Thiago Ferreira <ferreirazz056@gmail.com>
2025-08-25 22:40:40 -0300

    test4

--- Modelos/task_manager.py
+++ Modelos/task_manager.py
@@ -7,7 +7,7 @@ class TaskManager:
 
     def __init__(self):
         self.tarefas = []
-        #teste3
+        #teste4
 
     def adicionar_tarefa(self, titulo, descricao, prioridade="------"):
         nova_tarefa = Task(titulo, descricao, prioridade)
